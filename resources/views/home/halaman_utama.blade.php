9c1fdde14396e905fbd00851279bac7807c2f1df
Dinda Aulia <dindaaullia06@gmail.com>
2023-03-28 14:22:40 +0700

    revisi nav utama

--- resources/views/home/halaman_utama.blade.php
+++ resources/views/home/halaman_utama.blade.php
@@ -40,9 +40,9 @@
                         </button>
                     </div>  
                     <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row lg:self-center">
-                        <a class="px-4 py-4 text-sm font-semibold text-white border-b border-white md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none lg:mr-10 lg:self-center" href="#">Home</a>
+                        <a class="px-4 py-4 lg:text-sm text-xs font-semibold text-white border-b border-white md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none lg:mr-10 lg:self-center" href="#">Home</a>
                         <div @click.away="open = false" class="relative lg:self-center" x-data="{ open: false }">
-                            <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-4 lg:my-4 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline lg:mr-10 lg:self-center">
+                            <button @click="open = !open" class="flex flex-row items-center w-full px-4 lg:py-4 py-2 lg:my-4 mt-2 lg:text-sm text-xs font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline lg:mr-10 lg:self-center">
                                 <span>Produk</span>
                                 <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1 lg:self-center"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </button>
@@ -58,7 +58,7 @@
                         </div>
     
                         <div @click.away="open = false" class="relative" x-data="{ open: false }">
-                            <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-4 lg:my-4 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline lg:mr-10 lg:self-center">
+                            <button @click="open = !open" class="flex flex-row items-center w-full px-4 lg:py-4 py-2 lg:my-4 mt-2 lg:text-sm text-xs font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline lg:mr-10 lg:self-center">
                                 <span>Area Layanan</span>
                                 <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </button>
@@ -72,62 +72,60 @@
                             </div>
                         </div>
                         <div @click.away="open = false" class="relative" x-data="{ open: false }">
-                            <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-4 lg:my-4 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline lg:mr-10 lg:self-center">
+                            <button @click="open = !open" class="flex flex-row items-center w-full px-4 lg:py-4 py-2 lg:my-4 mt-2 lg:text-sm text-xs font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline lg:mr-10 lg:self-center">
                                 <span>Tingkatan</span>
                                 <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </button>
                             <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full md:max-w-screen-sm md:w-screen mt-2 origin-top-right z-30">
-                                <div class="px-4 py-2 pb-4 bg-white rounded-md shadow-lg dark-mode:bg-gray-700">
-                                  <div class="grid grid-cols-1 gap-4">
-                                    <a class="flex flex-row items-start rounded-lg bg-transparent p-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/sd">
-                                      <div class="bg-red-200 text-white rounded-lg p-3">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-sd">
+                                <div class="px-4 py-2 bg-white rounded-md shadow-lg dark-mode:bg-gray-700">
+                                  <div class="grid grid-cols-1 lg:gap-4 gap-1">
+                                    <a class="flex flex-row items-start rounded-lg bg-transparent px-2 py-1 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/sd">
+                                      <div class="bg-red-200 text-white rounded-lg p-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="lg:w-5 w-3 lg:h-5 h-3 text-sd">
                                             <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                                         </svg>
-                                          
                                       </div>
                                       <div class="ml-3">
-                                        <p class="text-base font-bold text-black">Sekolah Dasar</p>
-                                        <p class="text-base font-normal text-body">Program bimbingan belajar dan les privat untuk jenjang SD. </p>
+                                        <p class="lg:text-base text-sm font-bold text-black">Sekolah Dasar</p>
+                                        <p class="lg:text-base text-sm font-normal text-body">Program bimbingan belajar dan les privat untuk jenjang SD. </p>
                                       </div>
                                     </a>
         
-                                    <a class="flex flex-row items-start rounded-lg bg-transparent p-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/smp">
-                                      <div class="bg-green-200 text-white rounded-lg p-3">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-primary">
+                                    <a class="flex flex-row items-start rounded-lg bg-transparent px-2 py-1 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/smp">
+                                      <div class="bg-green-200 text-white rounded-lg p-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="lg:w-5 w-3 lg:h-5 h-3 text-primary">
                                             <path fill-rule="evenodd" d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z" clip-rule="evenodd" />
                                         </svg>
                                       </div>
                                       <div class="ml-3">
-                                        <p class="text-base font-bold text-black">Sekolah Menengah Pertama</p>
-                                        <p class="text-base font-normal text-body">Program bimbingan belajar dan les privat untuk jenjang SMP. </p>
+                                        <p class="lg:text-base text-sm font-bold text-black">Sekolah Menengah Pertama</p>
+                                        <p class="lg:text-base text-sm font-normal text-body">Program bimbingan belajar dan les privat untuk jenjang SMP. </p>
                                       </div>
                                     </a>
         
-                                    <a class="flex flex-row items-start rounded-lg bg-transparent p-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/sma">
-                                      <div class="bg-slate-300 text-white rounded-lg p-3">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-dark">
+                                    <a class="flex flex-row items-start rounded-lg bg-transparent px-2 py-1 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/sma">
+                                      <div class="bg-slate-300 text-white rounded-lg p-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="lg:w-5 w-3 lg:h-5 h-3 text-dark">
                                             <path fill-rule="evenodd" d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z" clip-rule="evenodd" />
                                         </svg>
                                       </div>
                                       <div class="ml-3">
-                                        <p class="text-base font-bold text-black">Sekolah Menengah Atas</p>
-                                        <p class="text-base font-normal text-body">Program bimbingan belajar dan les privat untuk jenjang SMA. </p>
+                                        <p class="lg:text-base text-sm font-bold text-black">Sekolah Menengah Atas</p>
+                                        <p class="lg:text-base text-sm font-normal text-body">Program bimbingan belajar dan les privat untuk jenjang SMA. </p>
                                       </div>
                                     </a>
     
-                                    <a class="flex flex-row items-start rounded-lg bg-transparent p-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/mahasiswa">
-                                        <div class="bg-slate-300 text-white rounded-lg p-3">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-mhs">
+                                    <a class="flex flex-row items-start rounded-lg bg-transparent px-2 py-1 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/mahasiswa">
+                                        <div class="bg-slate-300 text-white rounded-lg p-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="lg:w-5 w-3 lg:h-5 h-3 text-mhs">
                                                 <path d="M11.584 2.376a.75.75 0 01.832 0l9 6a.75.75 0 11-.832 1.248L12 3.901 3.416 9.624a.75.75 0 01-.832-1.248l9-6z" />
                                                 <path fill-rule="evenodd" d="M20.25 10.332v9.918H21a.75.75 0 010 1.5H3a.75.75 0 010-1.5h.75v-9.918a.75.75 0 01.634-.74A49.109 49.109 0 0112 9c2.59 0 5.134.202 7.616.592a.75.75 0 01.634.74zm-7.5 2.418a.75.75 0 00-1.5 0v6.75a.75.75 0 001.5 0v-6.75zm3-.75a.75.75 0 01.75.75v6.75a.75.75 0 01-1.5 0v-6.75a.75.75 0 01.75-.75zM9 12.75a.75.75 0 00-1.5 0v6.75a.75.75 0 001.5 0v-6.75z" clip-rule="evenodd" />
                                                 <path d="M12 7.875a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" />
                                               </svg>
-                                              
                                         </div>
                                         <div class="ml-3">
-                                          <p class="text-base font-bold text-black">Mahasiswa</p>
-                                          <p class="text-base font-normal text-body">Program bimbingan belajar dan les privat untuk jenjang Mahasiswa. </p>
+                                          <p class="lg:text-base text-sm font-bold text-black">Mahasiswa</p>
+                                          <p class="lg:text-base text-sm font-normal text-body">Program bimbingan belajar dan les privat untuk jenjang Mahasiswa. </p>
                                         </div>
                                       </a>
                                   </div>
@@ -135,11 +133,11 @@
                             </div>
                         </div> 
                         
-                        <a class="px-4 py-4 mt-2 text-sm font-semibold text-white md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none lg:mr-10 lg:self-center" href="#testi">Testimoni</a>
-                        <a class="px-4 py-4 mt-2 text-sm font-semibold text-white md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none lg:mr-16 lg:self-center" href="/news">News</a>
+                        <a class="px-4 lg:py-4 py-2 mt-2 text-sm font-semibold text-white md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none lg:mr-10 lg:self-center" href="#testi">Testimoni</a>
+                        <a class="px-4 lg:py-4 py-2 mt-2 text-sm font-semibold text-white md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none lg:mr-16 lg:self-center" href="/news">News</a>
                         @guest
                         <div class="lg:self-center lg:ml-56">
-                            <a class="text-white text-sm font-semibold hover:text-gray-900 focus:text-gray-900 focus:outline-none px-4 py-4 lg:mr-3 lg:self-center lg:text-right lg:right-0" href="/registrasi">Daftar</a>
+                            <a class="text-white text-sm font-semibold hover:text-gray-900 focus:text-gray-900 focus:outline-none px-4 lg:py-4 py-2 lg:mr-3 lg:self-center lg:text-right lg:right-0" href="/registrasi">Daftar</a>
                             <a class="text-white font-semibold text-sm border border-white lg:px-4 lg:py-2 py-2 lg:mt-5 mt-2 text-center rounded-lg justify-items-end w-[94px] h-[40px] lg:mr-3 lg:right-0" href="/login">Masuk</a>
                         </div>
                         @endguest
