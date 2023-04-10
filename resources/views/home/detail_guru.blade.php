da029da9f063e52b890010c6d758bfa6df902a16
[Fadel Mahamid Ahmad] <[fadelmahamid107@gmail.com]>
2023-04-10 16:29:39 +0700

    revisi

--- resources/views/home/detail_guru.blade.php
+++ resources/views/home/detail_guru.blade.php
@@ -303,7 +303,5 @@
     </div>
 </div>
 </section>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="{{ asset('/js/detail_guru.js') }}"></script>
 
 @endsection
